3462026740d97b2bba0b1c15c3048ef2d832941d
ebzna <domyaku@proton.me>
2024-03-15 15:17:10 +0200

    Added lsp-zero

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,61 +1,59 @@
-
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.wrap = false
--- disable defualt neovim statusline
--- vim.o.ls = 0
+vim.keymap.set("n", ";p", vim.cmd.Ex)
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.wrap = false
 vim.o.background = 'dark'
 vim.o.ignorecase = true
 vim.o.guicursor = 'n-v-c:block,i:hor1'
-vim.keymap.set("n", ";p", vim.cmd.Ex)
+vim.o.ls = 0	-- disable defualt neovim statusline
+vim.o.scl='no'	-- disable diagnostics sign column
 
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+	use {
+		'wbthomason/packer.nvim',
+		'ellisonleao/gruvbox.nvim',
+		'folke/tokyonight.nvim',
+		'nvim-treesitter/nvim-treesitter',
+		'christoomey/vim-tmux-navigator',
+		'nvim-telescope/telescope.nvim',
+		'nvim-lua/plenary.nvim',
+	}
 
-	use 'ellisonleao/gruvbox.nvim'
-	require('gruvbox').setup({ 
+	use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+	    {'williamboman/mason.nvim'},
+		{'williamboman/mason-lspconfig.nvim'},
+	    {'neovim/nvim-lspconfig'},
+	    {'hrsh7th/nvim-cmp'},
+		{'hrsh7th/cmp-nvim-lsp'},
+		{'L3MON4D3/LuaSnip'},
+	  }
+	}
+
+	require('gruvbox').setup({
 		transparent_mode = true ,
-		italic = { 
-			strings = false, 
-			emphasis = false, 
-			comments = false, 
-			operators = false, 
-			folds = false 
-		},
+		italic = { strings = false, emphasis = false, comments = false, operators = false, folds = false },
 	})
 
-	use 'folke/tokyonight.nvim'
-	require('tokyonight').setup({ 
-		style = 'night', 
-		styles = {
-			comments = { italic = false },
-    		keywords = { italic = false },
-		},
+	require('tokyonight').setup({
+		style = 'night',
+		styles = { comments = { italic = false }, keywords = { italic = false } },
 		transparent = true,
 		on_colors = function(colors) colors.fg = "#ffffff" end
 	})
+	vim.cmd('colorscheme tokyonight')
 
-	use 'nvim-treesitter/nvim-treesitter'
 	require'nvim-treesitter.configs'.setup {
-	  ensure_installed = { 
-		  "c",
-		  "lua",
-		  "vim",
-		  "vimdoc",
-		  "rust",
-		  "javascript",
-		  "typescript",
-		  "go",
-		  "python",
-	  },
+	  ensure_installed = { "c", "lua", "vim", "vimdoc", "rust", "javascript", "typescript", "go", "python" },
 	  sync_install = false,
 	  auto_install = true,
 	  ignore_install = {},
 	  highlight = {
 		enable = true,
-		disable = {},
 		disable = function(lang, buf)
 			local max_filesize = 100 * 1024 -- 100 KB
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -65,13 +63,49 @@ return require('packer').startup(function(use)
 	  },
 	}
 
-	use	'christoomey/vim-tmux-navigator'
-	use 'nvim-telescope/telescope.nvim'
-	use 'nvim-lua/plenary.nvim'
-
 	local builtin = require('telescope.builtin')
 	vim.keymap.set('n', ';o', builtin.find_files, {})
 	vim.keymap.set('n', ';l', builtin.live_grep, {})
 
-	vim.cmd('colorscheme tokyonight')
+	local lsp_zero = require('lsp-zero')
+
+	lsp_zero.on_attach(function(client, bufnr)
+	  lsp_zero.default_keymaps({buffer = bufnr})
+	end)
+
+	require('mason').setup({})
+	require('mason-lspconfig').setup({
+	  ensure_installed = {},
+	  handlers = {
+	    lsp_zero.default_setup,
+		lua_ls = function ()
+			require('lspconfig').lua_ls.setup({
+				settings = {
+					Lua = {
+      					diagnostics = { globals = { 'vim', 'require' } },
+					}
+				}
+			})
+		end,
+	  },
+	})
+
+	local cmp = require('cmp')
+	local cmp_action = require('lsp-zero').cmp_action()
+
+	cmp.setup({
+	  mapping = cmp.mapping.preset.insert({
+	    -- `Enter` key to confirm completion
+	    ['<CR>'] = cmp.mapping.confirm({select = false}),
+	    -- Ctrl+Space to trigger completion menu
+	    ['<C-Space>'] = cmp.mapping.complete(),
+	    -- Navigate between snippet placeholder
+	    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+	    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+	    -- Scroll up and down in the completion documentation
+	    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+	    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+	  })
+	})
+
 end)
